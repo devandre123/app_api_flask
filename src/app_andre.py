039be1849d0e1e9628d8cda194b0ddb49b37bcdd
andre <deolliveira@gmail.com>
2023-12-19 14:08:31 -0300

    updat sqlalchemy

--- src/app_andre.py
+++ src/app_andre.py
@@ -1,8 +1,40 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 databases = []
+
+def checkapi(Function):
+    if Function == 'getdatabases':
+        for i in databases:
+            if i == 'null':
+                return 301
+            else:
+                return 200
+
+class GetBases(Resource):
+    def get(__self__):
+
+        status_code = checkapi("getdatabases")
+
+        if (status_code != 200):
+            retjson={
+                'Mensagem': "Sem bases para execução",
+                'Status Code': status_code
+            }
+            return jsonify(retjson)
+
+        for i in databases:
+            ret={
+                'Banco de dados': i['bd'],
+                'tabela': i['tabela'],
+                'audiencia': i['audiencia'],
+                'midia': i['midia']
+            }
+        return jsonify(ret)
+
 @app.route('/', methods=["GET", "POST"])
 def page_home():
 
@@ -16,6 +48,8 @@ def page_home():
             
     return render_template('index.html', databases=databases)
 
+
+api.add_resource(GetBases, "/getdatabases")
 
 if __name__ == '__main__':
     app.run(debug=True)
