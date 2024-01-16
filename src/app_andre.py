93746643a112923f23f9124ccbd9d2051600952f
andre <deolliveira@gmail.com>
2024-01-16 16:00:31 -0300

    add_bd_sqlite3

--- src/app_andre.py
+++ src/app_andre.py
@@ -1,14 +1,45 @@
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import pandas as pd
+
+
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app_andre.sqlite3"
+db = SQLAlchemy(app)
+
 databases = []
+
+
+con = sqlite3.connect("/home/andre/Documents/projetos/app_api_flask/src/app_andre.sqlite3")
+
+cur = con.cursor()
+
+df = pd.read_sql_query('SELECT * FROM db_bases', con)
+
+
+class dbBases(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    database = db.Column(db.String(50))
+    tabela = db.Column(db.String(50))
+    audiencia = db.Column(db.String(50))
+    midia = db.Column(db.String(50))
+
+    def __init__(self,database,tabela,audiencia,midia):
+        self.database = database
+        self.tabela = tabela
+        self.audiencia = audiencia
+        self.midia = midia
+
+
 
 def checkapi(Function):
     if Function == 'getdatabases':
-        for i in databases:
+        for i in df.iterrows():
             if i == 'null':
                 return 301
             else:
@@ -25,19 +56,19 @@ class GetBases(Resource):
                 'Status Code': status_code
             }
             return jsonify(retjson)
-
-        for i in databases:
-            ret={
-                'Banco de dados': i['bd'],
-                'tabela': i['tabela'],
-                'audiencia': i['audiencia'],
-                'midia': i['midia']
-            }
-        return jsonify(ret)
+        
+        else:
+            #for index, i in df.iterrows():
+            all_qurey = [{'Banco de dados': i['database'],
+                        'tabela': i['tabela'],
+                        'audiencia': i['audiencia'],
+                        'midia': i['midia']} for index, i in df.iterrows()]
+            return jsonify(all_qurey)
 
 @app.route('/', methods=["GET", "POST"])
 def page_home():
 
+    '''
     if request.method == "POST":
         if request.form.get("bd") and request.form.get("tabela") and request.form.get("audiencia") and request.form.get("midia"):
             databases.append({"bd": request.form.get("bd"),\
@@ -45,11 +76,24 @@ def page_home():
                               "audiencia": request.form.get("audiencia"),\
                               "midia": request.form.get("midia")})
             print(databases)
-            
+    '''
+
+    banco =request.form.get("bd")
+    tabeladb = request.form.get("tabela")
+    audienciadb = request.form.get("audiencia")
+    midiadb = request.form.get("midia")
+
+    if request.method == "POST":
+
+        db_base = dbBases(banco, tabeladb, audienciadb, midiadb)
+        db.session.add(db_base)
+        db.session.commit()
+
     return render_template('index.html', databases=databases)
 
 
 api.add_resource(GetBases, "/getdatabases")
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
